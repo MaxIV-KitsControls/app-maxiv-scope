30ec5691dcd334ba6386b0d29c5971bac14fa749
Paul Bell <paubel@ec-kitslab-0.maxlab.lu.se>
2014-03-05 13:19:14 +0100

    all files for first installation

--- src/tgconf_scope/ui_measurements.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'measurements.ui'
-#
-# Created: Mon Feb 17 17:02:04 2014
-#      by: PyQt4 UI code generator 4.10.3
-#
-# WARNING! All changes made in this file will be lost!
-
