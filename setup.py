c91e95957e1ac46db55ff7e86dfce3a84bd9539e
Paul Bell <paubel@ec-kitslab-0.maxlab.lu.se>
2014-03-05 13:17:16 +0100

    all files for first installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = "taurusgui-scope",
+      version = "1.0",
+      description = "TaurusGUI for Rohde&Schwarz oscilloscope",
+      author = "Paul Bell",
+      author_email = "paul.bell@maxlab.lu.se",
+      license = "GPLv3",
+      url = "http://www.maxlab.lu.se",
+      package_dir = {'':'src',},
+      packages = ['tgconf_scope', 'tgconf_scope.panels'],
+      include_package_data=True,
+      package_data={'tgconf_scope': ['images/maxivlogo.png']},
+      scripts = ['scripts/ctScope']
+     )
+
