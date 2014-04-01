7401702d7759de3461883ffa06a36b312c6cfc23
Paul Bell <paubel@ec-kitslab-0.maxlab.lu.se>
2014-04-01 13:42:48 +0200

    v2.2.0 to deploy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "taurusgui-scope",
-      version = "1.2.0",
+      version = "2.2.0",
       description = "TaurusGUI for Rohde&Schwarz oscilloscope",
       author = "Paul Bell",
       author_email = "paul.bell@maxlab.lu.se",
