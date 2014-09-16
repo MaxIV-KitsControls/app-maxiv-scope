1dd899d5a111649283d3d419ccc19b05199ccf0d
Vincent Michel <vincent.michel@maxlab.lu.se>
2014-09-16 11:20:07 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "taurusgui-scope",
-      version = "0.0.1",
+      version = "3.0.1",
       description = "TaurusGUI for the different scopes",
       author = "Vincent MICHEL",
       license = "GPLv3",
