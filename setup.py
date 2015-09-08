a1b8f61c81b8e139bd8525fd0e399dd17a78ad46
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-09-08 16:25:35 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "taurusgui-scope",
-      version = "4.1.3",
-      description = "TaurusGUI for oscilloscopes",
-      author = "Vincent MICHEL",
-      license = "GPLv3",
-      packages = find_packages(),
+setup(name="taurusgui-scope",
+      version="4.1.3",
+      packages=find_packages(),
+      entry_points={'gui_scripts': ['ctscope = scopegui:main']},
+
       include_package_data=True,
       package_data={'': ['*.png']},
       data_files=[('/usr/share/applications', ['maxiv-scope.desktop'])],
-      scripts = ['scripts/ctscope']
-     )
 
+      license="GPLv3",
+      description="TaurusGUI for oscilloscopes",
+      author="Vincent Michel",
+      url="http://www.maxlab.lu.se",
+      )
